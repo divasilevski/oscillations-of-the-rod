8b013d6b4fe5c71c7fe90d036e1b5db78f2c4b75
divasilevski <divasilevski@gmail.com>
2019-11-20 00:57:16 +0300

    stabilizer matrix

--- scr/Regularization.js
+++ scr/Regularization.js
@@ -22,31 +22,100 @@ class Regularization {
         delta = 0,
         eps = 0.0001
     ) {
-        [
-            this.matrix,
-            this.right_part,
-            this.step,
-            this.left,
-            this.right,
-            this.p,
-            this.init_alpha,
-            this.h,
-            this.delta,
-            this.eps
-        ] = arguments;
+        let N = right_part.length;
+        let C = stabilizer(N, step, left, right);
+        console.log(C);
+    }
+
+    solution() {
+        return 1;
     }
 }
 
-function stabilizer(n, step, left = "Dirichle", right = "Dirichle") {
-    let diagonal = []; // n
-    let up_diagonal = []; // n - 1
-
+function stabilizer(n, step, left, right) {
+    let diagonal = [];
     let h = 1.0 / step / step;
     for (let i = 0; i < n; i++) diagonal.push(1 + 2 * h);
-    for (let i = 0; i < n - 1; i++) up_diagonal.push(-h);
 
     diagonal[0] = left == "Dirichle" ? 1 + 3 * h : 1 + h;
     diagonal[n - 1] = right == "Dirichle" ? 1 + 3 * h : 1 + h;
 
-    return { diagonal, up_diagonal };
+    let matrix = zeros(n);
+    for (let i = 0; i < n; i++) {
+        matrix[i][i] = diagonal[i];
+    }
+
+    // Верхняя и нижняя диагональ
+    for (let i = 1; i < n; i++) {
+        matrix[i - 1][i] = -h;
+        matrix[i][i - 1] = -h;
+    }
+
+    return matrix;
 }
+
+function zeros(N) {
+    // Создаем матрицу из нулей
+    let matrix = [];
+    for (let i = 0; i < N; i++) {
+        matrix.push([]);
+        for (let j = 0; j < N; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    return matrix;
+}
+
+/*****************************************************************************/
+function kernel(x, kappa, k, a) {
+    // Ядро интеграла
+    let gamma = Math.sqrt(a * a - kappa * kappa / (1 + k * k));
+    return (
+        -kappa *
+        kappa *
+        (1 - k * k) *
+        Math.sinh(gamma * x) *
+        Math.cosh(gamma * x) /
+        ((1 + k * k) *
+            (1 + k * k) *
+            gamma *
+            Math.cosh(gamma) *
+            Math.cosh(gamma))
+    );
+}
+
+const PI = 3.1415926538;
+const N = 10; // Количество точек
+let min_kappa = 0;
+let max_kappa = 1;
+let alpha = 1.0;
+let k = 1.0;
+
+let step_kappa = (max_kappa - min_kappa) / N;
+let h = 1 / N;
+f = i => sin(PI * i * h);
+
+let kappa;
+let row;
+let matrix = [];
+for (let i = 0; i < N; i++) {
+    kappa = min_kappa + step_kappa * i;
+    row = [];
+    for (let j = 0; j < N; j++)
+        row.push(h * kernel((j + 0.5) * h, kappa, k, alpha));
+    matrix.push(row);
+}
+
+let sum;
+let right_part = [];
+for (let i = 0; i < N; i++) {
+    sum = 0;
+    for (let j = 0; j < N; j++) {
+        sum += matrix[i][j] * f[j];
+    }
+    right_part.push(sum);
+}
+
+regularization = new Regularization(matrix, right_part, h);
+
+console.log(regularization.solution());
